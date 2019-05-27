2ed1f4c9ad4527ff544e694a904f02b7e25d7e0e
andreicostin <andreicostin@users.noreply.github.com>
2019-05-27 20:18:56 +0100

    Mixin created

---
+++ src/mixins.js
@@ -0,0 +1,24 @@
+const mixIn = {
+  data () {
+    return {
+      today: new Date()
+    }
+  },
+  beforeCreate () {
+    this.$store.dispatch('setJobs')
+  },
+  methods: {
+    // get the difference between date and current date in days
+    getNumberOfDays (from, to) {
+      const then = new Date(to)
+      const diff = then - from
+      return Math.ceil(diff / (1000 * 60 * 60 * 24))
+    },
+    reset () {
+      this.form.email = ''
+      this.form.jobs = null
+    }
+  }
+}
+
+export default mixIn
