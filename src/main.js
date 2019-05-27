472874211d7c152219bc1e99d3a2caaa7f4c8af7
andreicostin <andreicostin@users.noreply.github.com>
2019-05-27 20:14:16 +0100

    Bootstrap and firebase setup in the vue app

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import '@/firebase'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/scss/styles.scss'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = true
+
+Vue.use(BootstrapVue)
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
